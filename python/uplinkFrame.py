7eec7bc5476b6353ae2e8bf2de9fa2a053045084
Max Well <themeynoush@gmail.com>
2023-04-02 11:53:36 +0200

    correlation_plot

--- python/uplinkFrame.py
+++ python/uplinkFrame.py
@@ -1,6 +1,6 @@
-# write a program that asks the user to enter a sequece of numbers including only 0 and 1
-# and convert each 1 to -1, and each 0 to 1. The program should then print the results 
-# of the conversion. For example, if the user enters 1010, the program should print -1 1 -1 1.
+# Studying correlation computation between a FT and Preamble in Sigfox Network
+# Author: Meynoush - github: @themeynoush
+
 
 # create a function that converts 1 to -1 and 0 to 1
 def convert(num):
@@ -10,7 +10,9 @@ def convert(num):
         return 1
     
 # convert the preamble and frame type to a list of 1s and -1s
-frame_type = list(map(convert, '0000001101011'))
+frame_type = list(map(convert, '0b0001101011111'))
+# frame_type_2 = 0b0000010001101
+# frame_type_3 = 0b0001101011111
 preamble = list(map(convert, '1010101010101010101'))
 
 # correlate the preamble with the frame type
@@ -29,7 +31,7 @@ import numpy as np
 plt.plot(corr_result)
 plt.xlabel('Shift')
 plt.ylabel('Correlation Result')
-plt.title('Correlation of the 1st Frame Type and Preamble')
+plt.title('Correlation of the 3rd Frame Type and Preamble')
 plt.annotate('Peak correlation', xy=(peak_index, max(corr_result)), xytext=(peak_index, max(corr_result)+1),
                 arrowprops=dict(facecolor='red', shrink=0.05))
 plt.show()
@@ -41,5 +43,5 @@ for i in range(len(frame_type)):
         result = frame_type[i] * preamble[j]
         print(f'{i+j}\t{frame_type[i]}\t\t{preamble[j]}\t\t{result}')
 
-# Path: zeroToOne.py
+
 
