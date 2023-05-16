aa61afc00c5a24a0b66dfb19dc8877f2165ad2b2
Mair nousH <themeynoush@gmail.com>
2023-05-16 13:17:01 +0200

    userText

--- python/text.py
+++ python/text.py
@@ -1,4 +1,11 @@
-text = ['this is a text', 'this is another text']
+# write a program that prints out the text entered by the user
+# the program should print out the text in a list
+
+text = []
+#ask the user to enter a text
+text.append(input('Enter a text: '))
+#ask the user to enter another text
+text.append(input('Enter another text: '))
+
 for charachter in text:
     print(charachter)
-    
