a6c48dd5cab524f767fd97f1c8d1de54ab14b9e2
Mair nousH <themeynoush@gmail.com>
2023-06-08 17:37:09 +0200

    multiply two numbers

--- python/multiplyTwoNum.py
+++ python/multiplyTwoNum.py
@@ -2,13 +2,18 @@
 # Author: Meynoush
 
 # Define the mulitplication number
-def mulitply_numbers(num1, num2):
-    multipy = num1 * num2
+def multiply_numbers(num1, num2):
+    return num1 * num2
 
 # Ask the user to enter two numbers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = multiply_numbers(num1, num2)
 
 # Call the function and print the results:
-print("The mulitpcation result is: ", mulitply_numbers)
+print("The mulitpcation result is: ", result)
+
+
+
 
