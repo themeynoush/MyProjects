f932f325bcb54a21681d4d1ebaed22ce541a4f9e
Mair nousH <themeynoush@gmail.com>
2023-06-08 15:38:43 +0200

    muitply2numbers

---
+++ python/multiplyTwoNum.py
@@ -0,0 +1,14 @@
+# Write a program that asks the user to enter two numbers and mulitply them together
+# Author: Meynoush
+
+# Define the mulitplication number
+def mulitply_numbers(num1, num2):
+    multipy = num1 * num2
+
+# Ask the user to enter two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Call the function and print the results:
+print("The mulitpcation result is: ", mulitply_numbers)
+
